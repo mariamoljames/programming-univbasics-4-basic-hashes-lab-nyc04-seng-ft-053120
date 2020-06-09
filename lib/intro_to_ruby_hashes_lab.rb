8f50f8ce85ee9d9fb2328a220563541612414d30
Mariamol James <mariamol.james@gmail.com>
2020-06-09 03:52:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,14 @@ def new_hash
 end
 
 def my_hash
-person={maria:39}
+{maria:39}
 end
 
 def pioneer
-hash=[name:gracehopper]
+{name:gracehopper}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  {id:number}
 end
